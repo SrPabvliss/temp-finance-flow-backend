0df3274eae77b79323701cf23db8f2a956107a36
SrPabvliss <pablomartinvillacres@gmail.com>
2025-04-17 17:25:39 -0500

    fix: added async await on asynchronous operations. now using AppError class

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -2,38 +2,40 @@ import { Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { PrismaService } from 'src/prisma/prisma.service';
+import { AppError } from 'src/shared/app.error';
+import { Errors } from 'src/shared/errors';
 
 @Injectable()
 export class UsersService {
   constructor(private readonly prisma: PrismaService) {}
+
   async create(createUserDto: CreateUserDto) {
     const existingUser = await this.prisma.user.findUnique({
       where: { email: createUserDto.email },
     });
 
     if (existingUser) {
-      throw new Error('Email already exists');
+      throw new AppError('Email already exists', Errors.CONFLICT);
     }
     return this.prisma.user.create({
       data: createUserDto,
     });
   }
 
-  findOne(id: number) {
-    const existingUser = this.prisma.user.findUnique({
+  async findOne(id: number) {
+    const existingUser = await this.prisma.user.findUnique({
       where: { id },
     });
 
     if (!existingUser) {
-      throw new Error('User not found');
+      throw new AppError('User not found', Errors.NOT_FOUND);
     }
-    return this.prisma.user.findUnique({
-      where: { id },
-    });
+    return existingUser;
   }
 
-  update(id: number, updateUserDto: UpdateUserDto) {
-    return this.prisma.user.update({
+  async update(id: number, updateUserDto: UpdateUserDto) {
+    await this.findOne(id);
+    return await this.prisma.user.update({
       where: { id },
       data: updateUserDto,
     });
